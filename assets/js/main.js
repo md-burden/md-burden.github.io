dcf0590e00cea7b3b3977163185f017e3856d25d
Matthew Burden <mdburden@uncg.edu>
2024-06-10 11:35:22 -0400

    Updated index file

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function($) {
 
 
     /*======= Populate GitHub Projects *=======*/
-		let gitHubUsername = 'uncg-csc340';
+		let gitHubUsername = 'md-burden';
 
 		// Run GitHub API function, passing in the GitHub username
 		requestUserRepos(gitHubUsername)
